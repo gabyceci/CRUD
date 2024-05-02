73a05d9c8295f95f41de30d098ef731aaad51e4f
Luz Gaspario <20200448@ricaldone.edu.sv>
2024-05-02 15:26:19 -0600

    CRUD

---
+++ app/src/main/java/gabriela/arevalo/crudgabriela1a/MainActivity.kt
@@ -0,0 +1,51 @@
+package gabriela.arevalo.crudgabriela1a
+
+import Modelo.ClaseConexion
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //1- Mandar llamar todos los elementos de la vista
+        val txtNombre = findViewById<EditText>(R.id.txtNombre)
+        val txtPrecio = findViewById<EditText>(R.id.txtPrecio)
+        val txtCantidad = findViewById<EditText>(R.id.txtCantidad)
+        val btnAgregar = findViewById<Button>(R.id.btnAgregar)
+
+        //2- Programar el boton de agregar
+        btnAgregar.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO){
+                //Guardar datos
+                //1- Crear un objeto de la clase de conexion
+                val objConexion = ClaseConexion().cadenaConexion()
+
+
+                //2- Crear una variable que sea igual a un PrepareStatement
+                val addProducto = objConexion?.prepareStatement("insert into tbProductoss values(?,?,?)")!!
+                addProducto.setString(1, txtNombre.text.toString())
+                addProducto.setInt(2, txtPrecio.text.toString().toInt())
+                addProducto.setInt(3, txtCantidad.text.toString().toInt())
+                addProducto.executeUpdate()
+            }
+        }
+
+    }
+}
