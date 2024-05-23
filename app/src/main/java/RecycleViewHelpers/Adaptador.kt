d923097ad1f271be2a2a3e4d1a4c70bdd9580dc3
Pablo <20220021@ricaldone.edu.sv>
2024-05-23 15:41:09 -0600

    cambio3

--- app/src/main/java/RecycleViewHelpers/Adaptador.kt
+++ app/src/main/java/RecycleViewHelpers/Adaptador.kt
@@ -7,6 +7,14 @@ import androidx.recyclerview.widget.RecyclerView
 import gabriela.arevalo.crudgabriela1a.R
 
 class Adaptador(private var Datos: List<listaProductos>): RecyclerView.Adapter<ViewHolder>() {
+
+
+    fun actualizarRecyclerView(nuevaLista: List<listaProductos>){
+        Datos = nuevaLista
+        notifyDataSetChanged() //Notifica que hay datos nuevos
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val vista = LayoutInflater.from(parent.context).inflate(R.layout.activity_itam_card, parent, false)
 
